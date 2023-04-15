7eb3a5dd384dd92e0affa3dfffc857c9254af0fd
leerivera <leerivera80@gmail.com>
2023-04-14 20:39:18 -0400

    server db

--- fullstack-app/server.js
+++ fullstack-app/server.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const connectDB = require("./config/database");
+
+
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" });
+
+// Passport config
+// require("./config/passport")(passport);
+
+//Connect To Database
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT, () => {
+    console.log("Server is running, you better catch it!");
+  });
