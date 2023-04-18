14b43bf6455ce15e4db9ff7ce9d5121d058c6bd0
leerivera <leerivera80@gmail.com>
2023-04-18 12:58:20 -0400

    more

--- fullstack-app/connectfour/src/App.js
+++ fullstack-app/connectfour/src/App.js
@@ -5,16 +5,30 @@ function App() {
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+  return (
     <div className="App">
     <h1>Welcome</h1>
     <form>
       <label>Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>Password:
+        <input type="text" value={password} onChange={handlePasswordChange} />
       </label>
       <br />
       <button type="submit">Submit</button>
     </form>
     </div>
+  );
 
 }
 
